e675c35e4159f052f55885cc70cbedc98583f6f2
Daniel Stüwe <daniel.stuewe@mainbornwolff.de>
2023-06-13 11:58:34 +0200

    setup amplify api

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:garden_test/services/plant_service.dart';
-import 'package:garden_test/widgets/combined_button.dart';
-import 'package:garden_test/widgets/plant_list.dart';
 import 'package:provider/provider.dart';
 
+import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
 import 'pages/home/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    await Amplify.addPlugin(
+        AmplifyAPI(modelProvider: ModelProvider.instance)
+    );
+
+    try {
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      safePrint('An error occurred configuring Amplify: $e');
+    }
+  }
 
   // This widget is the root of your application.
   @override
@@ -26,4 +51,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
