cdfb222ead615cc00cfad860d13b553e5344ea3e
Daniel Stüwe <daniel.stuewe@mainbornwolff.de>
2023-06-15 19:22:08 +0200

    improve detail page using ChatGPT

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../models/Plant.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key, required this.plant});
+  const DetailPage({Key? key, required this.plant}) : super(key: key);
+
   final Plant plant;
 
   @override
   Widget build(BuildContext context) {
     final plantType = plant.hasPlantType;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(plant.name),
@@ -19,26 +22,104 @@ class DetailPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.only(left: 60, right: 8),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text('Species: ${plantType.species}'),
-          Flexible(
-            child: Text("Description: ${plantType.description}"),
-          ),
-          Text('Irrigation Frequency in Days: ${plantType.irrigationFrequencyInDays}'),
-          Text('Bloom Month: ${plantType.bloomMonths}'),
-          Row(
-            children: [
-              Icon(
-                plantType.edible ?? false
-                    ? Icons.spa_outlined
-                    : Icons.sick_outlined,
-              ),
-              Text('Edible: ${plantType.edible}'),
-            ],
-          ),
-          Text('Created at: ${plantType.createdAt}'),
-        ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildLabelRow(
+              label: 'Species',
+              icon: Icons.info_outline,
+              value: plantType.species,
+            ),
+            const SizedBox(height: 12),
+            _buildLabelRow(
+              label: 'Description',
+              icon: Icons.description_outlined,
+              value: plantType.description,
+              maxWidth: 500,
+            ),
+            const SizedBox(height: 12),
+            _buildLabelRow(
+              label: 'Irrigation Frequency in Days',
+              icon: Icons.water_outlined,
+              value: plantType.irrigationFrequencyInDays.toString(),
+            ),
+            const SizedBox(height: 12),
+            plantType.bloomMonths == null
+                ? const SizedBox.shrink()
+                : _buildLabelRow(
+                    label: 'Bloom Month',
+                    icon: Icons.grass_outlined,
+                    value: plantType.bloomMonths.toString(),
+                  ),
+            const SizedBox(height: 12),
+            _buildLabelRow(
+              label: 'Edible',
+              icon: plantType.edible ?? false
+                  ? Icons.spa_outlined
+                  : Icons.sick_outlined,
+              value: plantType.edible.toString(),
+            ),
+            const SizedBox(height: 12),
+            _buildLabelRow(
+              label: 'Created at',
+              icon: Icons.calendar_today_outlined,
+              value: _formatDateTime(plantType.createdAt?.getDateTimeInUtc()),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _buildLabelRow({
+    required String label,
+    required IconData icon,
+    required String value,
+    double maxWidth = double.infinity,
+  }) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(width: 8),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    icon,
+                    size: 20,
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    label,
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  const SizedBox(width: 29), // Empty space in the bottom-left corner
+                  Container(
+                    constraints: BoxConstraints(maxWidth: maxWidth),
+                    child: Text(
+                      value,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  String _formatDateTime(DateTime? dateTime) {
+    return dateTime == null ? "Unkown" : DateFormat.yMMMMd().format(dateTime);
   }
 }
